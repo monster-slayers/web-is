bc9fcac524d40e4f8bfe05589e5cc12f8f2763bc
David Kizivat <3535926+kizivat@users.noreply.github.com>
2017-10-23 23:14:21 +0200

    JobDao definition

--- web-is-persistence/src/main/java/cz/muni/fi/pa165/monsterslayers/dao/JobDao.java
+++ web-is-persistence/src/main/java/cz/muni/fi/pa165/monsterslayers/dao/JobDao.java
@@ -1,10 +1,41 @@
 package cz.muni.fi.pa165.monsterslayers.dao;
 
+import cz.muni.fi.pa165.monsterslayers.entities.Job;
+
+import java.util.List;
+
 /**
  * Job's data access object.
  *
  * @author David Kizivat
  */
 public interface JobDao {
-    // TODO: define this interface once the Job entity is added
+    /**
+     * Create new Job.
+     *
+     * @param job Job to be created
+     */
+    void create(Job job);
+
+    /**
+     * Deletes Job.
+     *
+     * @param job Job to be deleted
+     */
+    void delete(Job job);
+
+    /**
+     * Finds all Jobs.
+     *
+     * @return List of all Jobs
+     */
+    List<Job> findAll();
+
+    /**
+     * Finds Job by its Id.
+     *
+     * @param id Id of the Job to be found
+     * @return Object of Job with the given Id
+     */
+    Job findById(Long id);
 }
