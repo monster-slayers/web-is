207e22208f56de7e422c1047218df6a5d61e36a5
David Kizivat <3535926+kizivat@users.noreply.github.com>
2017-10-23 13:46:58 +0200

    Basic entity classes for User/Hero with needed enums

---
+++ web-is-persistence/src/main/java/cz/muni/fi/pa165/monsterslayers/entities/enums/RightsLevel.java
@@ -0,0 +1,4 @@
+package cz.muni.fi.pa165.monsterslayers.entities.enums;
+
+public enum RightsLevel {
+}
---
+++ web-is-persistence/src/main/java/cz/muni/fi/pa165/monsterslayers/entities/enums/UserStatus.java
@@ -0,0 +1,12 @@
+package cz.muni.fi.pa165.monsterslayers.entities.enums;
+
+/**
+ * Enumerates statuses that a User can be in.
+ *
+ * @author David Kizivat
+ */
+public enum UserStatus {
+    ACTIVE,
+    INACTIVE,
+    DECEASED
+}
