ccad7ca487bec0648d7fb003f0271ec1a92c68c1
HeRoOfTheHell <max.zp.ua@mail.ru>
2017-10-27 15:31:42 +0200

    MonsterType entity done

---
+++ web-is-persistence/src/main/java/cz/muni/fi/pa165/monsterslayers/entities/MonsterType.java
@@ -0,0 +1,89 @@
+package cz.muni.fi.pa165.monsterslayers.entities;
+
+import cz.muni.fi.pa165.monsterslayers.entities.enums.Elements;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+public class MonsterType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //hibernate needs it
+    public MonsterType() {
+    }
+
+    //konstruktor, pri vytvoreni objektu nastavi hodnoty atributu
+    public MonsterType(String name) {
+        this.name = name;
+    }
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    private String food;
+
+    private Set<Elements> weaknesses = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFood() {
+        return food;
+    }
+
+    public void setFood(String food) {
+        this.food = food;
+    }
+
+    public Set<Elements> getWeaknesses() {
+        return weaknesses;
+    }
+
+    public void setWeaknesses(Set<Elements> weaknesses) {
+        this.weaknesses = weaknesses;
+    }
+
+    public void addWeakness(Elements weakness) {
+        this.weaknesses.add(weakness);
+    }
+
+    public void removeWeakness(Elements weakness) {
+        this.weaknesses.remove(weakness);
+    }
+
+    public boolean hasWeakness(Elements weakness) {
+        return this.weaknesses.contains(weakness);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || !(o instanceof MonsterType)) return false;
+
+        MonsterType that = (MonsterType) o;
+
+        return getName().equals(that.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return getName().hashCode();
+    }
+}
