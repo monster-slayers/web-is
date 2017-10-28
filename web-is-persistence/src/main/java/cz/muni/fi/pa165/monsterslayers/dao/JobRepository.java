ae3b9c29f698f2dcf8361c978835964adbebab16
David Kizivat <3535926+kizivat@users.noreply.github.com>
2017-10-28 18:55:03 +0200

    Complete JobRepository

--- web-is-persistence/src/main/java/cz/muni/fi/pa165/monsterslayers/dao/JobRepository.java
+++ web-is-persistence/src/main/java/cz/muni/fi/pa165/monsterslayers/dao/JobRepository.java
@@ -1,4 +1,14 @@
 package cz.muni.fi.pa165.monsterslayers.dao;
 
-public interface JobRepository {
+import cz.muni.fi.pa165.monsterslayers.entities.Hero;
+import cz.muni.fi.pa165.monsterslayers.entities.Job;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Job repository extends Spring's CrudRepository to automatically implement data access operations.
+ *
+ * @author David Kizivat
+ */
+public interface JobRepository extends CrudRepository<Job, Long> {
+    Job getByAssignee(Hero assignee);
 }
