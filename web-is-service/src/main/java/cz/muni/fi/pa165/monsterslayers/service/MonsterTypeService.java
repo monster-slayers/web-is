d9240e42d375ae381b2dee812ad14ce25b50253b
Ondřej Budai <ondrej@budai.cz>
2017-11-20 14:07:23 +0100

    Add sample test with working Mockito and possibly Dozer

--- web-is-service/src/main/java/cz/muni/fi/pa165/monsterslayers/service/MonsterTypeService.java
+++ web-is-service/src/main/java/cz/muni/fi/pa165/monsterslayers/service/MonsterTypeService.java
@@ -3,7 +3,6 @@ package cz.muni.fi.pa165.monsterslayers.service;
 import cz.muni.fi.pa165.monsterslayers.entities.MonsterType;
 import org.springframework.stereotype.Service;
 
-@Service
 public interface MonsterTypeService {
     MonsterType findById(Long id);
     Iterable<MonsterType> findAll();
